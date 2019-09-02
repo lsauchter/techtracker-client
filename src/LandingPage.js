fba54f9489bd6579dc06fe98b3042d8d4c5421ed
Lauren Auchter <lsauchter@gmail.com>
2019-09-02 18:13:53 -0400

    Base layout and styling

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LandingPage.css';
 
 export default function LandingPage() {
     return (
         <>
-        <p>LandingPage</p>
+        <div className="pageLinks">
+            <Link to="/checkin"><button className="buttonLink">Check In</button></Link>
+            <Link to="/checkout"><button className="buttonLink">Check Out</button></Link>
+        </div>
+        <div className="info">
+            <p>Thanks for using TechTracker! Click the links above to enter your name and select the items you're checking in or out.
+            </p>
+        </div>
         </>
     )
 }
