70372786d124cd7e03425f9a0909ac845c44f13a
Lauren Auchter <lsauchter@gmail.com>
2019-09-02 20:44:27 -0400

    Added icons and now.json for zeit deployment

--- src/CheckOut.js
+++ src/CheckOut.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import TechContext from './TechContext';
 import './CheckOut.css';
 
 export default function CheckOut() {
+    const {users, inventory} = useContext(TechContext)
+
+    const names = users.map(user => {
+        return <option key={user.id}>{user.name}</option>
+    })
+
+    const computers = inventory.map(item => {
+        if (item.category === 'computer') {
+            return <li key={item.id}>
+                <input 
+                    type="checkbox"
+                    name={item.id}
+                    id={item.id}
+                />
+                <label
+                    className="checkbox"
+                    htmlFor={item.id}
+                >
+                    {item.name}
+                    <img src={item.image} />
+                </label>
+            </li>
+        }
+    })
+    
+    
     return (
         <>
-        <p>CheckOut</p>
+        <form className="checkForm">
+            <label htmlFor="name">Name:</label>
+            <select id="name">
+                {names}
+            </select>
+            <fieldset className="computers">
+                <legend>computers</legend>
+                <ul className="flex">
+                    {computers}
+                </ul>
+            </fieldset>
+        </form>
         </>
     )
 }
