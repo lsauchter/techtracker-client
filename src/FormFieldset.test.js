5af98296141aaab03060511da2db0a5f8c096c07
Lauren Auchter <lsauchter@gmail.com>
2019-09-20 15:14:21 -0400

    Passing smoke tests

--- src/FormFieldset.test.js
+++ src/FormFieldset.test.js
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom';
 import FormFieldset from './FormFieldset';
 
 it('renders without crashing', () => {
+  const computers = [{
+        id: 1,
+        name: 'Purple Mac',
+        quantity: 6,
+        quantityAvailable: 4,
+        category: 'computer',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/51et4LzjJxL._SL1000_.jpg'
+        },
+        {
+        id: 2,
+        name: 'Red Mac',
+        quantity: 6,
+        quantityAvailable: 4,
+        category: 'computer',
+        image: 'https://images-na.ssl-images-amazon.com/images/I/515I48-ZKKL._SL1000_.jpg'
+        }]
   const div = document.createElement('div');
-  ReactDOM.render(<FormFieldset />, div);
+  ReactDOM.render(<FormFieldset category="Computers" inventory={computers}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
